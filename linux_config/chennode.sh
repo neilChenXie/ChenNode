78669fb3d3164bbd88bf16ecfc9bd1b9759b8023
Chen Xie <xcmickey@gmail.com>
2015-07-07 01:30:01 -0700

    2015-07-07:01:30:01

---
+++ linux_config/chennode.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#chmod 755 /etc/init.d/foobar
+#update-rc.d foobar defaults 
+# The following part always gets executed.
+#echo "This part always gets executed"
+
+# The following part carries out specific functions depending on arguments.
+case "$1" in
+	start)
+		/usr/bin/forever ~/ChenNode/index.js
+		;;
+	stop)
+		#echo "Stopping foobar"
+		;;
+	*)
+		#echo "Usage: /etc/init.d/foobar {start|stop}"
+		exit 1
+		;;
+esac
+
+exit 0
